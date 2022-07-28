5f06ae7158efc46d9d1c2c73cc9abf35beed568d
Thierry <varelathierry@gmail.com>
2022-07-28 18:56:12 -0300

    exercicio 1 filter

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const filtro = species.filter((element) => ids.includes(element.id));
+  return filtro;
 }
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 
 module.exports = getSpeciesByIds;
