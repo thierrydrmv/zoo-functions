0c10c783d1701ddb2589ff1f1b50610b6ccf4852
Thierry <varelathierry@gmail.com>
2022-07-28 19:50:38 -0300

    exercicio 3 find

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let resultado;
+  if (!employeeName) {
+    resultado = {};
+  }
+  data.employees.find((element) => {
+    if (element.firstName === employeeName || element.lastName === employeeName) {
+      resultado = element;
+    }
+    return resultado;
+  });
+  return resultado;
 }
+console.log(getEmployeeByName('Nelson'));
 
 module.exports = getEmployeeByName;
