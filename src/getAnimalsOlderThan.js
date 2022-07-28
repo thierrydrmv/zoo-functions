4c029e99f8976191b5f608929f07fca45553e41e
Thierry <varelathierry@gmail.com>
2022-07-28 19:20:33 -0300

    exercicio 2 filter+every

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const filtro = species.filter((especie) => especie.name === animal);
+  return filtro.every((element, index) => element.residents[index].age > age);
 }
+
+console.log(getAnimalsOlderThan('tigers', 10));
 
 module.exports = getAnimalsOlderThan;
