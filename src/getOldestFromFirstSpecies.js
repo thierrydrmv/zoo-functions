287b3dbdc4dbee14e7d05f8eaea3ac26fe8c6451
Thierry <varelathierry@gmail.com>
2022-07-29 18:52:55 -0300

    ajuste object.values

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,8 +3,9 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(id) {
   const older = data.employees
     .filter((element) => element.id === id)[0].responsibleFor[0];
-  const filtro = data.species.filter((element) => older.includes(element.id))[0].residents;
-  return filtro.sort((a, b) => b.age - a.age)[0];
+  const filtro = data.species.filter((element) => older
+    .includes(element.id))[0].residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(filtro);
 }
 
 console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
