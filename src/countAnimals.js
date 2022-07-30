5c4ee0a4cabd2be5c9e84f66c000c75e4c8bfdc0
Thierry <varelathierry@gmail.com>
2022-07-29 21:42:31 -0300

    exercicio 5 incompleto

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const animais = data.species;
+  let resultado;
+  if (!animal) {
+    const arrayAnimal = animais.filter((element) => element.name);
+    resultado = arrayAnimal;
+  } else if (Object.keys(animal).length === 1) {
+    const arrayAnimal = animais.filter((element) => element.name.includes(Object.values(animal)));
+    resultado = arrayAnimal[0].residents.length;
+  } else if (Object.keys(animal).length === 2) {
+    const arrayAnimal = animais
+      .filter((element) => element.name.includes(Object.values(animal)[0]));
+    resultado = arrayAnimal[0].residents
+      .filter((element) => element.sex === Object.values(animal)[1]).length;
+  }
+  return resultado;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
