092a5910bd96af56c5dd4dc549d43c50c9e636c1
Thierry <varelathierry@gmail.com>
2022-07-30 10:03:12 -0300

    exercicio 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,22 +1,41 @@
 const data = require('../data/zoo_data');
 
+function createArray() {
+  const animais = data.species;
+  const array = [];
+  animais.map((element) => {
+    for (let i = 0; i < element.residents.length - 1; i += 1) {
+      if (element.residents) {
+        array.push(element.name);
+      }
+    }
+    return array;
+  });
+  const arrayAnimais = array.reduce((acumulado, atual) => {
+    if (acumulado[atual] === undefined) {
+      acumulado[atual] = 1;
+    }
+    acumulado[atual] += 1;
+    return acumulado;
+  }, {});
+  return arrayAnimais;
+}
+
 function countAnimals(animal) {
   const animais = data.species;
-  let resultado;
   if (!animal) {
-    const arrayAnimal = animais.filter((element) => element.name);
-    resultado = arrayAnimal;
-  } else if (Object.keys(animal).length === 1) {
-    const arrayAnimal = animais.filter((element) => element.name.includes(Object.values(animal)));
-    resultado = arrayAnimal[0].residents.length;
-  } else if (Object.keys(animal).length === 2) {
-    const arrayAnimal = animais
-      .filter((element) => element.name.includes(Object.values(animal)[0]));
-    resultado = arrayAnimal[0].residents
+    return createArray();
+  } if (Object.keys(animal).length === 1) {
+    return animais
+      .filter((element) => element.name.includes(Object.values(animal)))[0].residents.length;
+  } if (Object.keys(animal).length === 2) {
+    return animais
+      .filter((element) => element.name.includes(Object.values(animal)[0]))[0].residents
       .filter((element) => element.sex === Object.values(animal)[1]).length;
   }
-  return resultado;
 }
+
+// console.log(countAnimals());
 
 console.log(countAnimals());
 
