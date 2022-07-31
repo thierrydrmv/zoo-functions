64288887317bc032023961a158bd62f61cef06b7
Thierry <varelathierry@gmail.com>
2022-07-31 09:56:46 -0300

    exercicio 8 completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,48 @@
+const { company } = require('faker/lib/locales/en');
+const en = require('faker/lib/locales/en');
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // your code here
+function createArray(array) {
+  const arrayAge = [];
+  array.map((element) => {
+    if (element.age < 18) {
+      arrayAge.push('child');
+    } else if (element.age >= 18 && element.age < 50) {
+      arrayAge.push('adult');
+    } else {
+      arrayAge.push('senior');
+    }
+    return arrayAge;
+  });
+  return arrayAge;
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function countEntrants(array) {
+  const arrayAge = createArray(array);
+  const arrayFinal = arrayAge.reduce((acumulado, atual) => {
+    const copia = { ...acumulado };
+    if (copia[atual] === undefined) {
+      copia[atual] = 0;
+    }
+    copia[atual] += 1;
+    return copia;
+  }, {});
+  return arrayFinal;
 }
+
+function calculateEntry(array) {
+  if (!array || Object.keys(array).length === 0) {
+    return 0;
+  }
+  const idade = createArray(array);
+  const preco = data.prices;
+  const arrayfinal = [];
+  for (let i = 0; i < idade.length; i += 1) {
+    arrayfinal.push(preco[idade[i]]);
+  }
+  return parseFloat(arrayfinal.reduce((total, atual) => total + atual).toFixed(2));
+}
+
+console.log(calculateEntry([{ name: 'asdas', age: 5 }, { name: 'asdas', age: 50 }]));
 
 module.exports = { calculateEntry, countEntrants };
